68ac37edf637c7bfd3da7a0e3f17e71e9f867fb5
LiWei1214 <lailiwei2938@gmail.com>
2025-06-25 15:48:07 +0800

    Remove deprecated folder

--- deprecated/ProtectedRoutes.js
+++
@@ -1,9 +0,0 @@
-function ensureAuthenticated(req, res, next) {
-    if (req.isAuthenticated()) {
-    return next();
-    }
-    res.redirect('/login');
-    }
-   app.get('/protected', ensureAuthenticated, (req, res) => {
-    res.send('This is a protected route');
-    });
