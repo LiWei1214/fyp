dca16ffb6aac98497ff22d65a5c4143ef5e3a862
LiWei1214 <lailiwei2938@gmail.com>
2025-06-25 17:25:02 +0800

    Fix routing for GitHub Pages with basename

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import LecturerDashboard from './pages/LecturerDashboard';
@@ -15,20 +15,21 @@ import UploadMaterial from './pages/UploadMaterial';
 import EditMaterial from './pages/EditMaterial';
 import OnlineResources from './pages/OnlineResources';
 import StudentQuiz from './pages/StudentQuiz';
-import {ThemeProvider} from './context/ThemeContext';
-import {SearchProvider} from './context/SearchContext';
+import { ThemeProvider } from './context/ThemeContext';
+import { SearchProvider } from './context/SearchContext';
 
 function App() {
   return (
     <SearchProvider>
       <ThemeProvider>
         <UserProvider>
-          <Router>
+          <Router basename="/fyp">
             <Routes>
               <Route
                 element={
                   <ProtectedRoute allowedRoles={['student', 'lecturer']} />
-                }>
+                }
+              >
                 <Route path="/profile" element={<ProfilePage />} />
               </Route>
               <Route exact path="/" element={<HomePage />} />
