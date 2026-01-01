696e18664314513efd1de34528911d76f1486b02
LiWei1214 <lailiwei2938@gmail.com>
2026-01-02 00:23:23 +0800

    Change to import HashRouter

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import LecturerDashboard from './pages/LecturerDashboard';
@@ -23,7 +23,7 @@ function App() {
     <SearchProvider>
       <ThemeProvider>
         <UserProvider>
-          <Router basename="/fyp">
+          <Router>
             <Routes>
               <Route
                 element={
